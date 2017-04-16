ee3f78bc955641802f3a6e99d4ccc9c97aea5e67
Qu Jinghua <893510847@qq.com>
2017-04-16 21:50:02 +0800

    JS与HTML完全分离

--- JS图片库/example.js
+++ JS图片库/example.js
@@ -16,11 +16,48 @@
 //  }
 
 function showPic(whichpic){
+    if(!document.getElementById("placeholder")) return false;
     var source = whichpic.getAttribute("href");
     var placeholder = document.getElementById("placeholder");
+    if(placeholder.nodeName != "IMG") return false;
     placeholder.setAttribute("src",source);
-    var text = whichpic.getAttribute("title");
-    var description = document.getElementById("description");
-    // alert(description.childNodes[0].nodeValue);
-    description.firstChild.nodeValue = text;
+    if(document.getElementById("description")){
+        if(whichpic.getAttribute("title")){
+            var text = whichpic.getAttribute("title");
+        } else{
+            var text = "";
+        }
+        var description = document.getElementById("description");
+        // alert(description.childNodes[0].nodeValue);
+        if(description.firstChild.nodeType == 3){
+            description.firstChild.nodeValue = text;
+        }
+    }  
 }
+function prepareGallery(){
+    if(!document.getElementsByTagName) return false;
+    if(!document.getElementById) return false;
+    if(!document.getElementById("imagegallery")) return false;
+    var gallery = document.getElementById("imagegallery");
+    var links = gallery.getElementsByTagName("a"); 
+    for(var i=0;i<links.length;i++){
+        links[i].onclick = function(){
+            showPic(this);
+            return !(!showPic(this));
+            // showPic(this);
+            // return false;
+        }
+    }
+}
+function addLoadEvent(func){
+    var oldonload = window.onload;
+    if(typeof window.onload != 'function'){
+        window.onload = func;
+    }else{
+        window.onload = function(){
+            oldonload();
+            func();
+        }
+    }
+}
+addLoadEvent(prepareGallery);
